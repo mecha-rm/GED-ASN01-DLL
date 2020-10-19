144c388647a571cb59b15609743bb8f87a42c20e
Roderick Montague <roderick.montague243@gmail.com>
2020-10-19 01:29:35 -0400

    File System Complete (Not Tested Yet)
    Working on saving/loading data file. I'll test it before exporting the DLL and putting it into Unity.

--- FileSystem.cpp
+++ FileSystem.cpp
@@ -1,4 +1,5 @@
 #include "FileSystem.h"
+#include <iostream>
 
 // constructor
 FileSystem::FileSystem()
@@ -10,6 +11,16 @@ FileSystem::~FileSystem()
 {
     // closes file upon system termination if it's not closed already.
     file.close();
+
+    // not open
+    mode = 0;
+}
+
+// open for reading
+bool FileSystem::OpenForReading(const char* filePath)
+{
+    return OpenForReading(std::string(filePath));
+    delete[] filePath;
 }
 
 // opens a file for reading
@@ -18,6 +29,10 @@ bool FileSystem::OpenForReading(std::string filePath)
     // closes the file
     file.close();
 
+    // if there is no period, there cannot be a file extension
+    if (filePath.find(".") == std::string::npos)
+        return false;
+
     // opens the file for reading
     file.open(filePath, std::ios::in);
 
@@ -25,8 +40,33 @@ bool FileSystem::OpenForReading(std::string filePath)
     if (!file)
         return false;
 
+    // save file path
+    this->filePath = filePath;
+
+    // reading mode
+    mode = 1;
+
     // returns 'true' since the file is now open.
     return true;
+}
+
+// opens a file for writing.
+bool FileSystem::OpenForWriting(const char* filePath)
+{
+    return OpenForWriting(filePath, false);
+}
+
+// open for writing
+bool FileSystem::OpenForWriting(const char* filePath, bool createFile)
+{
+    return OpenForWriting(std::string(filePath), false);
+    delete[] filePath;
+}
+
+// opens a file for writing.
+bool FileSystem::OpenForWriting(std::string filePath)
+{
+    return OpenForWriting(filePath, false);
 }
 
 
@@ -36,39 +76,243 @@ bool FileSystem::OpenForWriting(std::string filePath, bool createFile)
     // closes the file
     file.close();
 
+    // if there is no period, there cannot be a file extension
+    if (filePath.find(".") == std::string::npos)
+        return false;
+
     // opens the file for writing.
     file.open(filePath, std::ios::out);
 
     // if the file is not open, then a 'false' is returned. 
-    if (!file)
-        return false;
+    if (!file) // file failed to open
+    {
+        if (createFile) // writes line to file.
+        {
+            // writes to the file and closes it.
+            file.write("X", 1);
+            file.close();
+
+            // re-opens the file and clears out all contents.
+            file.open(filePath, std::ios::out);
+            ClearFileContents(); 
+        }
+        else
+        {
+            file.close();
+            return false;
+        }
+    }
+
+    // save file path
+    this->filePath = filePath;
+
+    // writing mode
+    mode = 2;
 
     // returns 'true' since the file is now open.
     return true;
 }
 
+// returns the file path
+const char* FileSystem::GetFilePath() const
+{
+    return filePath.c_str();
+}
+
+// returns the file path as a string
+std::string FileSystem::GetFilePathAsString() const
+{
+    return filePath;
+}
+
 // checks if the file is open.
-bool FileSystem::FileOpen()
+bool FileSystem::FileOpen() const
 {
     return file.is_open();
 }
 
-// writes a line
-void FileSystem::WriteLine(char line[], int SIZE)
+// returns 'true' if the file is open for reading.
+bool FileSystem::IsReadable() const
 {
-    file.write(line, SIZE);
+    return (mode == 1);
 }
 
-// save line
-// void FileSystem::WriteLine(char line[], int SIZE, int insert)
-// {
-// 
-// }
+// returns 'true' if the file is open for writing.
+bool FileSystem::IsWritable() const
+{
+    return (mode == 2);
+}
+
+// reads a line
+const char* FileSystem::ReadLine(int size, int seekg)
+{
+    // size is invalid
+    if (size <= 0)
+        return nullptr;
+
+    // seek line
+    if (seekg >= 0)
+        file.seekg(seekg);
+
+    char* data = new char[size];
+
+    // reads file
+    file.read(data, size);
+
+    // saves the data
+    std::string str = std::string(data);
+    lines.push_back(str);
+
+    delete[] data;
+}
+
+// writes a line
+void FileSystem::WriteLine(const char* line, int size, int seekp)
+{
+    // size is invalid
+    if (size <= 0)
+        return;
+
+    // seek line
+    if (seekp >= 0)
+        file.seekp(seekp);
+
+    file.write(line, size);
+    file.write("\n", 1);
+
+    // saves the data
+    std::string str = std::string(line);
+    lines.push_back(str);
+
+    delete[] line;
+}
+
+// gets the line count
+int FileSystem::GetLineCount() const
+{
+    return lines.size();
+}
+
+// adds a line to the array
+void FileSystem::AddLine(const char* arr)
+{
+    std::string str = std::string(arr);
+    lines.push_back(str);
+    delete[] arr;
+}
+
+// removes a line
+void FileSystem::RemoveLine(const char* arr)
+{
+    std::string str = std::string(arr);
+    
+    // finds value and deletes it.
+    for (int i = 0; i < lines.size(); i++)
+    {
+        if (lines[i] == str) // line found
+        {
+            lines.erase(lines.begin() + i); // removed 
+            break; // breaks out of loop
+        }
+    }
+
+    delete[] arr;
+}
+
+// removes line based on index
+void FileSystem::RemoveLine(int index)
+{
+    // out of bounds
+    if (index >= 0 && index < lines.size())
+        lines.erase(lines.begin() + index);
+}
+
+// gets the line at the provided index
+const char* FileSystem::GetLine(int index) const
+{
+    return (index >= 0 && index < lines.size()) ? lines[index].c_str() : "";
+}
+
+// returns line as string
+std::string FileSystem::GetLineAsString(int index)
+{
+    return (index >= 0 && index < lines.size()) ? lines[index] : "";
+}
+
+// reads all lines
+void FileSystem::ReadAllLines()
+{
+    // if the file isn't open, or there's something wrong with reading from it.
+    if (!file.is_open() || !file || mode != 1)
+        return;
+
+    std::string line; // line variable
+
+    lines.clear(); // clears out all existing lines
+    file.seekg(std::ios::beg); // next character to be gotten
+
+    while (std::getline(file, line)) // loops through all lines
+    {
+        lines.push_back(line); // gets the line
+    }
+
+    // returns back to start of file.
+    file.seekg(std::ios::beg);
+}
+
+// writes all lines in a file. This clears all previous contents.
+void FileSystem::WriteAllLines()
+{
+    // file isn't open, it's invalid, or it's not in write mode.
+    if (!file.is_open() || !file || mode != 2)
+        return;
+    
+    // clears out all file contents
+    ClearFileContents();
+
+    // writes all lines in file
+    for (int i = 0; i < lines.size(); i++)
+    {
+        std::string str = lines[i] + "\n"; // add new line character, which increases char count by 1.
+
+        file.write(str.c_str(), str.size());
+    }
+
+}
+
+// clears out all file contents
+void FileSystem::ClearFileContents()
+{
+    if (filePath == "")
+        return;
+
+    file.close();
+
+    // opens the file and clears out the data.
+    switch (mode)
+    {
+    case 1: // reading
+        file.open(filePath, std::ios::in | std::ios::trunc);
+        break;
+    case 2: // writing
+        file.open(filePath, std::ios::out | std::ios::trunc);
+        break;
+    default:
+        mode = 0; // mode wasn't set
+    }
+}
+
+// clears out all lines
+void FileSystem::ClearAllLines()
+{
+    lines.clear();
+}
 
 // closes the file.
 void FileSystem::CloseFile()
 {
     file.close();
+    mode = 0;
 }
 
 // const char* FileSystem::GetLine(int i)
--- FileSystem.h
+++ FileSystem.h
@@ -2,7 +2,11 @@
 
 #include "PluginSettings.h"
 #include <fstream>
+#include <string>
 #include <vector>
+
+// TODO: the idea behind this system is that lines are passed in, and that when the user is finished they all get put into the file at once.
+// as such, some functions own't be used.
 
 // file system class
 // this clears out the entire file and rewrites it entirely if set to write.
@@ -14,32 +18,77 @@ public:
 
 	// destructor
 	~FileSystem();
+	
+	// open for reading
+	bool OpenForReading(const char* filePath);
 
 	// open the file for reading. If the file is currently open, it is closed.
 	// if no file exists, 'false' is returned.
 	bool OpenForReading(std::string filePath);
+
+	// open for writing. No file is generated if it doesn't exist.
+	bool OpenForWriting(const char* filePath);
+
+	// open for writing. Open to create the file if it doesn't exist yet.
+	bool OpenForWriting(const char* filePath, bool createFile);
+
+	// opens a file for writing.
+	bool OpenForWriting(std::string filePath);
 
 	// open the file for writing. If the file is currently open, it is closed.
 	// if the file doesn't exist, or if the file cannot be opened, 'false' is returned.
 	// if 'createFile' is set to 'true', an empty file is made at the provided location if it cannot be foun.
 	bool OpenForWriting(std::string filePath, bool createFile);
 
+	// returns the file path
+	const char* GetFilePath() const;
+
+	// gets the file path as a string
+	std::string GetFilePathAsString() const;
+
 	// returns 'true' if the file is currently open.
-	bool FileOpen();
+	bool FileOpen() const;
+
+	// returns 'true' if the file is open and is readable.
+	bool IsReadable() const;
+
+	// returns 'ture' if the file is open and is writable.
+	bool IsWritable() const;
+
+	// Record, WriteLine, and ReadLine functions will likely be deleted.
 
 	// gets a line from the 
 	// char[] GetLine(int index);
 
-	// saves a line to be written to the file.
-	// this does not actually write the line. Call WriteAllLines to write all lines to the files.
-	void WriteLine(char line[], int SIZE);
+	// returns the line count.
+	int GetLineCount() const;
 
-	// saves a line to be written to the file.
-	// this does not actually write the line. Call WriteAllLines to write all lines to the files.
-	// this inserts the line at the provided index. 
-	// If the index exceeds the vector size (i.e. it would leave empty spaces), it's placed at the end.
-	// void WriteLine(char line[], int SIZE, int insert);
+	// adds a line to be written to the file.
+	void AddLine(const char* arr);
 
+	// removes a line based on its string
+	void RemoveLine(const char* arr);
+
+	// removes a line based on its index
+	void RemoveLine(int index);
+
+	// returns the line
+	const char* GetLine(int index) const;
+
+	// returns the line as a string. This cannot be used as a DLL, since this data type does not transfer over.
+	std::string GetLineAsString(int index);
+
+	// reads in all lines, saving them to variables. These lines can then be read in.
+	void ReadAllLines();
+
+	// writes all lines for the file system. This clears all previous contents.
+	void WriteAllLines();
+
+	// clears out all file contents. This does not clear existing lines.
+	void ClearFileContents();
+
+	// clears out all line data
+	void ClearAllLines();
 
 	// closes the file
 	void CloseFile();
@@ -50,11 +99,27 @@ public:
 
 
 private:
+	// reads a line received from the file directly.
+	const char* ReadLine(int size, int seekg);
+
+	// saves a line to be written to the file.
+	// this does not actually write the line. Call WriteAllLines to write all lines to the files.
+	void WriteLine(const char* line, int size, int seekp);
+
+	// keeps track of mode the file is in (0 = none, 1 = reading, 2 = writing)
+	int mode;
+
+	// record size
+	int recordSize = 1024;
+
+	// file name
+	std::string filePath = "";
+
 	// file
 	std::fstream file;
 
 	// vector of lines for the file
-	std::vector<char[]> lines;
+	std::vector<std::string> lines;
 
 protected:
 };
